bf92b4023cf48a2346f2a1ec9e97bf1761ac87ec
lioY3 <974098492@qq.com>
2021-01-17 15:21:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/teaknowledge/ui/version/VersionFragment.java
@@ -0,0 +1,38 @@
+package com.example.teaknowledge.ui.version;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.teaknowledge.R;
+
+public class VersionFragment extends Fragment {
+
+    private VersionViewModel mViewModel;
+
+    public static VersionFragment newInstance() {
+        return new VersionFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_version, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(VersionViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/teaknowledge/ui/version/VersionViewModel.java
@@ -0,0 +1,7 @@
+package com.example.teaknowledge.ui.version;
+
+import androidx.lifecycle.ViewModel;
+
+public class VersionViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
